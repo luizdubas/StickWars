c2b926124855b9f34aff573676c08b95fd7b7711
Luiz <luizdubas@gmail.com>
2013-08-04 12:14:19 -0300

    commit com o projeto,e o modelo criado até agora

---
+++ StickWars/Assets/Scripts/AbstractSource.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AbstractSource : MonoBehaviour, IMaterialSource {
+	public int _amount = 1;
+	public int _squareSize = 1;
+	public MaterialType _type;
+	public AnimationType _animation;
+	
+	#region Behaviour
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	#endregion
+	
+	#region IMaterialSource implementation
+	
+	public void CollectSource (ref int quantity)
+	{
+		if(_amount - quantity < 0)
+			quantity = _amount;
+		_amount -= quantity;
+	}
+	
+	#endregion
+}
---
+++ StickWars/Assets/Scripts/Interfaces.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Tipos de animaçao possiveis para os materiais
+//Chop seria a animaçao para 'arvores'
+//Mine e a animaçao para coletar ouro ou os circulos para criar os personagens
+//Collect seria para coletar agua, que nao tera nesse
+public enum AnimationType { Chop, Mine, Collect }
+
+//Tipos de materiais possiveis no jogo
+//Stick seriam as arvores, nela se coleta os pauzinhos para criar personagens e construçoes
+//Circle e como o Gold no Age of Empires, so que nele se coleta os circulos necessarios para criar os personagens
+public enum MaterialType { Stick, Circle, Gold }
+
+public interface IMaterialSource
+{
+	void CollectSource(ref int quantity);
+}
