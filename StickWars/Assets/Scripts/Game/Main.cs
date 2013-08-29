8830a64cb919b7b25b9c899bdf00555da5ab49b0
Jonathan <jonathanblacknight@gmail.com>
2013-08-29 03:57:55 -0300

    Adicionei as coisas na minha cena no WalkScenario.unity mudei bastante de coisa de posição e re-associei alguns scripts.

---
+++ StickWars/Assets/Scripts/Game/Main.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Main : MonoBehaviour {
+
+private PathFinding pathFinder;
+	private List<Square> path = null;
+	private int index = 0;
+	private bool walk;
+	private float time;
+	
+	private float walkVelocity = 0.15f;
+	private GameObject hero;
+	
+	private Map map;
+	private CameraControllerInGame cameraController;
+	
+	private bool debug = true;
+	
+	void Start () {
+		//hero = GameObject.Find("hero");
+		
+		createMap();
+		
+		startCameraController();
+		
+		//pathFinder = new SimpleAStar(map, false);
+		
+		walk = false;
+		time = 0;
+	}
+	
+	void startCameraController(){
+		
+		cameraController = GameObject.Find("Main Camera").GetComponent<CameraControllerInGame>();
+		
+		cameraController.setCameraPosition( new Vector3( 250, 50, -200 ) );
+		cameraController.setCameraRotation( new Vector3( 50, 180, 0 ) );
+		
+		cameraController.setMargins( 40, -70, -40, -20 );
+		cameraController.setMinMaxZoom( 10, 40 );
+		
+		cameraController.setMap( map );
+		
+		cameraController.activate();
+	}
+	
+	void createMap(){
+		map = GameObject.Find("ground").GetComponent<Map>();
+		
+		map.SizeX = 100;
+		map.SizeY = 100;
+		
+		map.SquareWidth = 5;
+		map.SquareHeight = 5;
+		map.MapGameObject = GameObject.Find( "ground" );
+		
+		map.buildMap();
+		
+		map.createRandomsObstacles( 1000, map.ObstaclePrefab );
+	}
+	
+	void Update () {
+
+		if( walk ){
+			time += Time.deltaTime;
+			if( time >= walkVelocity && path.Count > 0 ){
+				time -= walkVelocity;
+				
+				Square posicao = path[index];
+				index++;
+				
+				hero.transform.position = new Vector3(posicao.Center.x , hero.transform.position.y, posicao.Center.y );
+				
+				if( index == path.Count){
+					walk = false;
+					time = 0;
+					index = 0;
+					
+					path.Clear();
+				}
+			}
+		}
+		
+		/*
+		if( Input.GetButtonDown("Fire2") && !walk){
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			bool right = false;
+			
+			if( Physics.Raycast( ray, out hit) ){
+				pathFinder.ClearLogic();
+				right = pathFinder.PathFind( map.getCoordinatesByWorldPosition( hero.transform.position ) , map.getCoordinatesByWorldPosition( hit.point ) );
+
+				if( right ){
+					path = pathFinder.getPath();
+					
+					if( debug ){
+						
+					}
+					
+					index = 0;
+					time = 0;
+					walk = true;
+				}
+			}	
+		}
+		*/
+
+	}
+	
+	void drawPath(){
+		
+	}
+}
